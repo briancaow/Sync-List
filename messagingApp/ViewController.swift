15e36037e08617f920244b05a7eacaba0efb657b
Brian Cao <brian.cao@icloud.com>
2019-02-18 22:53:44 -0800

    Initial Commit

---
+++ messagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messagingApp
+//
+//  Created by Brian Cao on 2/18/19.
+//  Copyright © 2019 Brian Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
